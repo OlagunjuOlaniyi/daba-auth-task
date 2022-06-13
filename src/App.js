2d30bde0e1333dea58aafdf67daafb9e7103f125
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-06-13 12:19:07 +0100

    Updates app.js file

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Constant from "./components/Constant";
 import Welcome from "./pages/welcome/Welcome";
 import Login from "./pages/login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Register from "./pages/register/Register";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
         </Routes>
       </BrowserRouter>
     </>
