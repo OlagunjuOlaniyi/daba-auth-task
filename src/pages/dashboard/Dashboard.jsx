750601af7434e793a6ebe7a4538671be68db7702
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-06-15 11:58:27 +0100

    adding logout functionalities to dashboard

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -1,13 +1,23 @@
 import React, { useContext } from "react";
 import "./Dashboard.css";
 import AuthContext from "../../context/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
 export const Dashboard = () => {
   const { auth } = useContext(AuthContext);
+
+  const navigate = useNavigate();
+
+  const handleLogOut = () => {
+    window.localStorage.clear();
+    navigate("/");
+  };
+
   return (
     <div className="dashboard">
       <div>
         <h1>Welcome {auth.firstName || localStorage.getItem("firstName")}</h1>
+        <button className="btn text-danger" onClick={handleLogOut}>Logout</button>
       </div>
     </div>
   );
