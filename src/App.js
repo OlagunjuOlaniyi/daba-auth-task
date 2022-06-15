7ce1b54261c9d520e8c858103f27249f2fbac96e
OlagunjuOlaniyi <olagunjuolaniyi512@gmail.com>
2022-06-15 08:45:36 +0100

    updating App.css and App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
 import Constant from "./components/Constant";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// pages
 import Welcome from "./pages/welcome/Welcome";
 import Login from "./pages/login/Login";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Register from "./pages/register/Register";
+import NoPage from "./pages/nopage/NoPage";
+import Dashboard from "./pages/dashboard/Dashboard";
 
 function App() {
   return (
@@ -14,6 +18,8 @@ function App() {
           <Route path="/" element={<Welcome />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="*" element={<NoPage />} />
         </Routes>
       </BrowserRouter>
     </>
